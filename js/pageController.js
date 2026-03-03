4bc54e4bd5c6cb8f41ef24e4d5f115e6515e94ab
Patrick Carver <patrickcarver@gmail.com>
2026-03-02 18:14:32 -0600

    try to use the cache in the loadPage

--- js/pageController.js
+++ js/pageController.js
@@ -5,21 +5,31 @@ export class PageController {
   #host = null;
   #pageData = null;
   #apiClient = null;
-  #pageCache = null;
+  #cache = null;
   #url = null;
 
   constructor(host, config) {
     this.#host = host;
     host.addController(this);
-    this.#pageCache = new Map();
+
+    this.#cache = new Map();
     this.#apiClient = new ApiClient(config);
     this.#url = config.url;
   }
 
   async loadPage({ page = 1, pageSize = 12 } = {}) {
     try {
-      const response = await this.#apiClient.fetchWithRetry({ page, pageSize });
-      return mapPageData(response, this.#url);
+      if (this.#cache.has(page) && this.#cache.get(page) != null) {
+        return this.#cache.get(page);
+      } else {
+        const response = await this.#apiClient.fetchWithRetry({
+          page,
+          pageSize,
+        });
+        const pageData = mapPageData(response, this.#url);
+        this.#cache.set(page, pageData);
+        return pageData;
+      }
     } catch (error) {
       console.error(error);
     }
